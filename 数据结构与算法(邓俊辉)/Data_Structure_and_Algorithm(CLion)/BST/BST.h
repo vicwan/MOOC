f408c19fac76c75d8d1e3f6fe507885df3aeb749
万伟琦 <wan.weiqi@zte.com.cn>
2018-01-30 16:35:33 +0800

    BST 查找，增加，删除节点的接口基本完成

--- 数据结构与算法(邓俊辉)/Data_Structure_and_Algorithm(CLion)/BST/BST.h
+++ 数据结构与算法(邓俊辉)/Data_Structure_and_Algorithm(CLion)/BST/BST.h
@@ -43,6 +43,38 @@ protected:
         }
     }
 
+    BinNodePosi(T) __removeAt( BinNodePosi(T) &x, BinNodePosi(T) &hot ) {
+        BinNodePosi(T) w = x;
+        BinNodePosi(T) succ = nullptr;
+        if( !x->_lChild ) {
+            x = x->_rChild;
+            succ = x;
+        } else if( !x->_rChild ) {
+            x = x->_lChild;
+            succ = x;
+        } else {
+            w = w->succ();
+            swap(x->_data, w->_data);
+            BinNodePosi(T) u = w->_parent;
+            // 隔离被删除节点，设置父节点的孩子，完成单向连接
+            succ = w->_rChild;
+            if( u == x ) {
+                u->_rChild = succ;
+            } else {
+                u->_lChild = succ;
+            }
+        }
+        // 记录 hot 节点
+        hot = w->_parent;
+        // 若删除节点是叶子节点，那么 succ 为 nullptr，自然也不需要设置其父亲
+        if( succ ) {
+        // 设置顶替的右子树根节点的父亲，至此完成双相连接
+            succ->_parent = hot;
+        }
+        // 释放被删除节点，此处未做
+        return succ;
+    }
+
 public:
 
     BinNodePosi(T) search( T const& e ) {
@@ -67,7 +99,15 @@ public:
     }
 
     bool remove( T const& e ) {
-        
+        // 如果节点不存在，直接 return
+        BinNodePosi(T) x = search(e);
+        if( !x ) return false;
+        // 实施删除
+        __removeAt(x, _hot);
+        // 更新规模，父节点高度
+        _size--;
+        updateHeightAbove(_hot);
+        return true;
     }
 };
 
